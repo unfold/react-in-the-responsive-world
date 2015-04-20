9cab0b914b4b6f4083db4fd9888b5a38931e5f4b
Simen Brekken <simen@unfold.no>
2015-04-20 18:44:46 +0200

    Clean up.

--- src/utils/getConnectionPixelRatio.js
+++
@@ -1,8 +0,0 @@
-const SLOW_CONNECTION_TYPE_PATTERN = /(^([23]g?)$)/
-
-export default function() {
-  const connection = navigator.connection
-  const slow = connection && SLOW_CONNECTION_TYPE_PATTERN.test(connection.type)
-
-  return (!slow && window.devicePixelRatio) || 1
-}
